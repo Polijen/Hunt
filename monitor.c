05fbeb818cb5c540e25bae11ae65af3a8518c00f
Polijen <victor4rushwars@gmail.com>
2025-05-17 19:02:23 +0300

    Add files via upload

--- monitor.c
+++ monitor.c
@@ -16,23 +16,25 @@ void handle_usr2(int sig) {
         system(cmd);
     }
 
-    fclose(f);
+    if ((fclose(f)) != 0){
+        perror("Erroare de inchide e fisierului de\n");
+    }
 }
 
 void handle_term(int sig) {
     printf("[MONITOR] Shutting down after 2 seconds...\n");
-    usleep(2000000);
+    usleep(20000); 
     exit(0);
 }
 
 int main() {
-    signal(SIGUSR2, handle_usr2);
+    signal(SIGUSR2, handle_usr2); //sa modific in sigaction !!!!!!!!!!!!
     signal(SIGTERM, handle_term);
 
     printf("[MONITOR] Running with PID %d\n", getpid());
 
     while (1) {
-        pause();
+        pause(); 
     }
 
     return 0;
